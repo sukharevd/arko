a3f94bf7fd60b42b6f93c22f23c9d0b0c77a3ce0
Dmitriy Sukharev <sukharevd@gmail.com>
2012-06-24 22:20:50 +0300

    Added copyright information.

--- bricktable.h
+++ bricktable.h
@@ -1,3 +1,26 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2010-2012 Dmitriy Sukharev.
+ ** All rights reserved.
+ ** Contact: Dmitriy Sukharev (ecuna@mail.ru)
+ **
+ ** This file is part of Arko.
+ **
+ ** Arko is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Arko is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Arko.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+
 // bricktable.h
 #ifndef BRICKTABLE_H
 #define BRICKTABLE_H
